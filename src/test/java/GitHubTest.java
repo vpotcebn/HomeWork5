41dcf1c8abf60f4f1ae56f4e76b42c3c389e5ced
Vlad Potcebnev <pocvlad@gmail.com>
2023-03-07 00:11:32 +0300

    Added GitHubTest and HerokuappTest

---
+++ src/test/java/GitHubTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubTest {
+    @Test
+    void hoverTest() {
+        open("https://github.com/");
+        $(".Header-old").$(byText("Solutions")).hover();
+        $("[href='/enterprise']").click();
+        $(".enterprise-hero").shouldHave(text("Build like the best"));
+
+    }
+}
---
+++ src/test/java/HerokuappTest.java
@@ -0,0 +1,18 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HerokuappTest {
+    @Test
+    void hoverTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        SelenideElement elementA = $("#column-a");
+        elementA.dragAndDropTo($("#column-b"));
+        elementA.shouldHave(exactText("B"));
+
+        sleep(5000);
+
+    }
+}
